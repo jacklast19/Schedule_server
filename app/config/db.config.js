683d94db652028e1547cecc42b3035190eedb567
jacklast19 <s6203052412181@email.kmutnb.ac.th>
2023-07-25 22:24:30 +0700

    first commit
    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url: "mongodb+srv://user2:AvWdZ2F3VjtjMtKR@cluster0.jkjcwvg.mongodb.net",
+  db: "ScheduleDB"
+};
---
+++ app/models/model.js
@@ -0,0 +1,31 @@
+module.exports = mongoose => {
+  var schema = mongoose.Schema(
+    {
+      username: String,
+      password: String,
+      firstname: String,
+      lastname: String,
+      departmentDetail: {
+        role: String,
+        salary: Number,
+        department: String
+      },
+      status: {
+        role: String,
+        active: Boolean
+      },
+      position: {type:String,default:'member'},
+      published: Boolean
+    },
+    { timestamps: true }
+  );
+
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Tutorial = mongoose.model("user", schema);
+  return Tutorial;
+};
